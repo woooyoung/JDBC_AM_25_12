bfa18c07eb34b65cbc98a746386f46a4b49f7877
woooyoung <axdsw121@gmail.com>
2025-12-12 17:19:40 +0900

    게시글 목록 조회 데이터를 DB로부터 가져오도록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,8 +12,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int lastArticleId = 0;
-
-        List<Article> articles = new ArrayList<>();
 
         while (true) {
             System.out.print("명령어 > ");
@@ -33,10 +28,7 @@ public class Main {
                 System.out.print("내용 : ");
                 String body = sc.nextLine().trim();
 
-                Article article = new Article(id, title, body);
-
                 lastArticleId++;
-                System.out.println(article);
                 System.out.println(id + "번 글이 작성되었습니다");
 
                 /// //////////////////////////////////////////////
@@ -85,10 +77,70 @@ public class Main {
                 }
             } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+                ResultSet rs = null;
+
+                List<Article> articles = new ArrayList<>();
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "SELECT *";
+                    sql += " FROM article";
+                    sql += " ORDER BY id DESC";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+                    rs = pstmt.executeQuery(sql);
+
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String regDate = rs.getString("regDate");
+                        String updateDate = rs.getString("updateDate");
+                        String title = rs.getString("title");
+                        String body = rs.getString("body");
+                        Article article = new Article(id, regDate, updateDate, title, body);
+                        articles.add(article);
+                    }
+
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (rs != null && !rs.isClosed()) {
+                            rs.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
                 if (articles.size() == 0) {
                     System.out.println("게시글 없음");
                     continue;
                 }
+
                 System.out.println("   번호    /    제목");
                 for (Article article : articles) {
                     System.out.printf("   %d    /   %s\n", article.getId(), article.getTitle());
