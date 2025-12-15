3fd5e6f5c681287e781910231dc537c20db93834
woooyoung <axdsw121@gmail.com>
2025-12-15 10:25:16 +0900

    게시글 수정 기능 구현, 글의 유무는 체크하지 않음

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -61,15 +61,15 @@ public class Main {
                     System.out.println("에러 : " + e);
                 } finally {
                     try {
-                        if (conn != null && !conn.isClosed()) {
-                            conn.close();
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
                         }
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
                     try {
-                        if (pstmt != null && !pstmt.isClosed()) {
-                            pstmt.close();
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
                         }
                     } catch (SQLException e) {
                         e.printStackTrace();
@@ -144,6 +144,74 @@ public class Main {
                 System.out.println("   번호    /    제목");
                 for (Article article : articles) {
                     System.out.printf("   %d    /   %s\n", article.getId(), article.getTitle());
+                }
+            } else if (cmd.startsWith("article modify")) {
+                int id = 0;
+
+                try {
+                    id = Integer.parseInt(cmd.split(" ")[2]);
+                } catch (Exception e) {
+                    System.out.println("정수 입력해");
+                    continue;
+                }
+
+                // 있는지 없는지
+
+                System.out.println("==글 수정==");
+                System.out.print("새 제목 : ");
+                String title = sc.nextLine().trim();
+                System.out.print("새 내용 : ");
+                String body = sc.nextLine().trim();
+
+                /// //////////////////////////////////////////////
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/JDBC_AM_25_12?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "UPDATE article";
+                    sql += " SET updateDate = NOW()";
+                    if (title.length() > 0) {
+                        sql += ", title = '" + title + "'";
+                    }
+                    if (body.length() > 0) {
+                        sql += ", `body` = '" + body + "'";
+                    }
+                    sql += " WHERE id = " + id + ";";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    int affectedRow = pstmt.executeUpdate();
+
+                    System.out.println("affectedRow = " + affectedRow);
+
+                    System.out.println(id + "번 글이 수정되었습니다");
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
